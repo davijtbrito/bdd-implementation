496f23a78caa13d27986e6bbae8e24119113705a
davibrito1218 <davibrito1218@gmail.com>
2024-04-12 21:39:57 +0100

    comments

--- bdd-implementation/src/test/java/FirstTest.java
+++ bdd-implementation/src/test/java/FirstTest.java
@@ -6,15 +6,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FirstTest {
     public static void main(String[] args){
 
+        //connectiong with the webdriver
         System.setProperty("webdriver.chrome.driver", "bdd-implementation/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
+        //defining timeouts
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().timeouts().setScriptTimeout(2, TimeUnit.MINUTES);
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 
+        //navigating to a page
         driver.get("http://goodle.com");
 
+        //ending browser
         driver.close();
     }
 }
